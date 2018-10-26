4c9acda1ca77cfba66f1e91eff16edba8c06b546
lvxw <2622478542@qq.com>
2018-10-26 22:41:09 +0800

    comment

--- globalObj/process.js
+++
@@ -1,9 +0,0 @@
-console.log(process.execPath)
-console.log(global.process.argv)
-var pids = process.pid;
-setInterval(function () {
-
-    console.log(pids)
-},1000);
-console.log(pids)
-process.exit(0)
--- globalObj/stdin_stdout.js
+++
@@ -1,5 +0,0 @@
-process.stdout.write("请输入：\n");
-process.stdin.resume();
-process.stdin.on('data',function (data) {
-    console.log(data.toString())
-});
